a5e88007e8c8a4c4a981ffb985dc92e746ed8bb7
enzinxz <enzopontesdonascimento@gmail.com>
2026-06-03 16:54:49 -0300

    Exercicio 42 - Encerrado

--- php/desafios/aula-40-desafio/index.php
+++ php/desafios/aula-40-desafio/index.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 $a = 1237.754;
 $b = 664.562;
@@ -8,13 +8,46 @@ $d = 19;
 
 if (is_float($a)) {
     echo "É um float! " . "$a" . "<br> <br>";
+} else {
+    echo "Não é um float!";
+}
+if (is_float($b)) {
+    echo "É um float! " . "$b" . "<br> <br>";
+} else {
+    echo "Não é um float!";
+}
+if (is_float($c)) {
+    echo "É um float! " . "$c" . "<br> <br>";
+} else {
+    echo "Não é um float!";
+}
+if (is_int($d)) {
+    echo "É um inteiro!" . "$d" . "<br> <br>";
+} else {
+    echo "Não é um Inteiro " . "$d";
 }
 
-echo "É um float: " . $b . "<br> <br>";
+ /*
+ // Método do otavio que ficou bem melhor
 
-echo "É um float: " . $c . "<br> <br>";
+ $z = 333;
 
-echo "É um Inteiro " . "$d"
+$list = [$a, $b, $c, $d];
+ 
+ echo "<br> <br><br><br><br>";
 
+$list = [$a, $b, $c, $d, $z];
 
-?>
+foreach ($list as $num) {
+    if (is_float($num)) {
+        echo "É um float! " . "$num" . "<br> <br>";
+    } elseif (is_int($num)) {
+        echo "É um inteiro!" . "$num" . "<br> <br>";
+    } elseif (!is_int($num)) {
+        echo "Não é um Inteiro " . "$num <br>" ;
+    } elseif (!is_float($num)) {         
+        echo "Não é um float! <br>";
+    }
+}
+
+*/
---
+++ php/secao-3/aula-41/index.php
@@ -0,0 +1,11 @@
+<?php 
+
+    echo "Testando texto de \"aspas duplas\" <BR><BR>";
+    
+    echo 'Testando texto de \'aspas simples\' <BR><BR>';
+
+    $idade = 15;
+    echo "Ele tem $idade anos."
+    
+
+?>
