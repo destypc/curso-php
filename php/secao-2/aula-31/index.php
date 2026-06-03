fcc591b5bb07575102b429c68cf2919e6353e802
enzinxz <enzopontesdonascimento@gmail.com>
2026-06-03 14:22:33 -0300

    Exercicio 32 - Encerrado

---
+++ php/secao-2/aula-31/index.php
@@ -0,0 +1,32 @@
+<?php 
+
+// Pessoa
+/*
+    Cabelo
+        Olho
+            Perna
+                Braço
+                    Orgãos
+                        Pele
+                            Nariz
+                        Unha
+                    Veias
+                Cílio
+            Orelhas
+        Músculos
+    Sobrancelha    
+*/
+
+// Nome de uma pessoa
+$nome = "Enzo ";
+
+// Idade da pessoa
+$idade = 19;
+
+// Profissão da pessoa
+$profissao = "Programador ";
+
+echo $nome;
+echo $idade;
+echo $profissao;
+?>
