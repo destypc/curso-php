28b092ab20ea291e5732bed1697137e276931ad5
enzinxz <enzopontesdonascimento@gmail.com>
2026-06-03 16:12:58 -0300

    Desafio Aula 40 - Encerrado

---
+++ php/desafios/aula-40-desafio/index.php
@@ -0,0 +1,20 @@
+<?php 
+
+$a = 1237.754;
+$b = 664.562;
+$c = 7342.123;
+$d = 19;
+
+
+if (is_float($a)) {
+    echo "É um float! " . "$a" . "<br> <br>";
+}
+
+echo "É um float: " . $b . "<br> <br>";
+
+echo "É um float: " . $c . "<br> <br>";
+
+echo "É um Inteiro " . "$d"
+
+
+?>
