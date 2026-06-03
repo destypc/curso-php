eee3cd60ada93f64c73e32fc0186b7bbef904c6d
enzinxz <enzopontesdonascimento@gmail.com>
2026-06-03 18:02:11 -0300

    Aula 47 - Encerrada

---
+++ php/secao-3/aula-47/index.php
@@ -0,0 +1,21 @@
+<?php 
+
+$arr = [
+'nome' => 'Enzo',
+'idade' => '15',
+'Irmãos' => '2',
+'Cor' => 'Branco',
+];
+
+echo $arr['nome'];
+echo "<BR>";
+print_r($arr);
+echo "<BR>";
+echo $arr['nome'];
+
+$arrAssoc = ['chave' => 'valor', 'bool' => true];
+
+echo "<BR>";
+print_r($arrAssoc);
+
+?>
