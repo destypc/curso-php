f8d440ab488dce091dcc80bb2c43624261c2b108
enzinxz <enzopontesdonascimento@gmail.com>
2026-06-05 16:09:52 -0300

    Desafio Aula 58 - Encerrada

---
+++ php/desafios/secao-4-desafio/aula-57-desafio/index.php
@@ -0,0 +1,19 @@
+<?php 
+
+$nome = "Enzo";
+$idade = 15;
+$animal = 'Billy';
+$itens = [
+    'nome' => 'Enzo',
+    'idade' => 15,
+    'animal' => 'Billy',
+];
+
+echo "<strong> Enzinxz </strong>";
+echo "<hr>";
+echo "<BR>";
+echo $idade;
+echo "<hr>";
+echo "<BR>";
+echo $itens['animal'];
+?>
---
+++ php/secao-4/aula-56/index.php
@@ -0,0 +1,19 @@
+<?php 
+
+$teste = "Algum texto";
+echo $teste;
+
+echo "<BR>";
+
+$num = 10;
+echo $num;
+
+echo "<BR>";
+
+$_nome = "Enzo";
+echo $_nome;
+
+
+
+
+?>
