8fa161e4e7b5d7906adf08f23bc07ae1c1e10921
enzinxz <enzopontesdonascimento@gmail.com>
2026-06-08 17:11:06 -0300

    Aula 79 - Encerrada

---
+++ php/secao-5/aula-79/index.php
@@ -0,0 +1,15 @@
+<?php 
+
+echo "Testando a concatenação";
+echo "<BR>";
+echo "<BR>";
+echo "Testando" . " a " . "Concatenação";
+echo "<BR>";
+echo "<BR>";
+
+$t = "Testando";
+$c = "Concatenação";
+
+echo $t . " a " . $c;
+
+?>
