9743e0740ae3f15ffdbe764221c522a0238d6442
enzinxz <enzopontesdonascimento@gmail.com>
2026-06-08 17:25:40 -0300

    Desafio aula 85 = Encerrado

---
+++ php/desafios/secao-5-desafio/aula-84-desafio/index.php
@@ -0,0 +1,21 @@
+<?php
+
+$a = 2;
+$b = 2;
+$c = "laele";
+
+if ($a == $b) {
+    echo "É verdadeiro";
+    echo "<BR>";
+} else {
+    echo "É falso";
+    echo "<BR>";
+}
+
+if ($a == $c) {
+    echo "É verdadeiro";
+    echo "<BR>";
+} else {
+    echo "É falso";
+    echo "<BR>";
+}
