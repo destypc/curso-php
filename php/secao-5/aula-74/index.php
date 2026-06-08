007e8e3107f9ed01f10ea28650cfac820dffa236
enzinxz <enzopontesdonascimento@gmail.com>
2026-06-08 16:15:10 -0300

    Desafio aula 75 - Encerrado

---
+++ php/secao-5/aula-74/index.php
@@ -0,0 +1,16 @@
+<?php 
+
+$soma = 5 + 5;
+echo $soma . "<BR>";
+
+$subtracao = 10 - 2;
+echo $subtracao . "<BR>";
+
+$multi = 2 * 199;
+echo $multi . "<BR>";
+
+$divisao = 123 / 48;
+echo $divisao . "<BR>";
+
+
+?>
