fd72802b38f23ec17a515cf48e5af53686b26e9f
enzinxz <enzopontesdonascimento@gmail.com>
2026-06-08 17:37:41 -0300

    Desafio aula 94 - Encerrada

---
+++ php/desafios/secao-5-desafio/aula-94-desafio/index.php
@@ -0,0 +1,27 @@
+<?php 
+
+if (15 > 5 || "João" === "João") {
+    echo "TRUE";
+    echo "<BR>";
+} else {
+    echo "FALSE";
+    echo "<BR>";
+}
+
+if ("teste" > 5 || 1) {
+    echo "TRUE";
+    echo "<BR>";
+} else {
+    echo "FALSE";
+    echo "<BR>";
+}
+
+if (2 == 3 || 5 >= 3) {
+    echo "TRUE";
+    echo "<BR>";
+} else {
+    echo "FALSE";
+    echo "<BR>";
+}
+
+?>
