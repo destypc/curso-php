0fae187abad1631b094e11654a9fe2e9e975f24e
enzinxz <enzopontesdonascimento@gmail.com>
2026-06-08 15:06:31 -0300

    Desafio aula 72 - Encerrado

---
+++ php/desafios/secao-5-desafio/aula-71-desafio/index.php
@@ -0,0 +1,16 @@
+<?php
+function br(){
+    echo "<br>";
+}
+
+$soma1 = 2 - 2 * (70 / 7);
+$soma2 = 20 - 87;
+$soma3 =  4489 / 67;
+$somaAll = $soma1 + $soma2 + $soma3;
+echo $soma1;
+br();
+echo $soma2;
+br();
+echo $soma3;
+br();
+echo $somaAll;
---
+++ php/secao-5/aula-70/index.php
@@ -0,0 +1,30 @@
+<?php 
+
+echo 3 + 2 * 5;
+echo "<BR>";
+echo "<BR>";
+echo (3 + 2) * 5;
+echo "<BR>";
+echo "<BR>";
+echo 5 + 2 / 10;
+echo "<BR>";
+echo "<BR>";
+
+$a = 5;
+$b = 2;
+$c = 10;
+
+echo $a + $b / $c;
+echo "<BR>";
+echo "<BR>";
+echo $c + $b / $a;
+echo "<BR>";
+echo "<BR>";
+
+$d = $a * $b * $c;
+
+echo $d;
+
+
+
+?>
