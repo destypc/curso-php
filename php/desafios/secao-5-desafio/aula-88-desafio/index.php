c5dc97705884d0cbb7f25f639cf0e5ed3c9ebe84
enzinxz <enzopontesdonascimento@gmail.com>
2026-06-08 17:30:26 -0300

    Desafio aula 88 - Encerrada

---
+++ php/desafios/secao-5-desafio/aula-88-desafio/index.php
@@ -0,0 +1,36 @@
+<?php
+
+$a = 5;
+$b = 3;
+
+if ($a == $b) {
+    echo "true";
+    echo "<BR>";
+} else {
+    echo "false";
+    echo "<BR>";
+}
+
+if ($a != $b) {
+    echo "true";
+    echo "<BR>";
+} else {
+    echo "false";
+    echo "<BR>";
+}
+
+if ($a === $b) {
+    echo "true";
+    echo "<BR>";
+} else {
+    echo "false";
+    echo "<BR>";
+}
+
+if ($a !== $b) {
+    echo "true";
+    echo "<BR>";
+} else {
+    echo "false";
+    echo "<BR>";
+}
