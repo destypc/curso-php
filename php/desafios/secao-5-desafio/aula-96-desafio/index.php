f52e9ebd83063b6f4a23aa0906365a1cc703e0e4
enzinxz <enzopontesdonascimento@gmail.com>
2026-06-08 17:41:59 -0300

    Desafio aula 97 - Encerrada

---
+++ php/desafios/secao-5-desafio/aula-96-desafio/index.php
@@ -0,0 +1,26 @@
+<?php 
+
+if (12 < 5 || "João" === "João") {
+    echo "TRUE";
+    echo "<BR>";
+} else {
+    echo "FALSE";
+    echo "<BR>";
+}
+
+if (1 > 5 || 1) {
+    echo "TRUE";
+    echo "<BR>";
+} else {
+    echo "FALSE";
+    echo "<BR>";
+}
+
+if (20 === "20" && 51 >= 31) {
+    echo "TRUE";
+    echo "<BR>";
+} else {
+    echo "FALSE";
+    echo "<BR>";
+}
+?>
