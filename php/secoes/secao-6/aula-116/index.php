4e8373a617928c974391e9f8d126a7f7fb1438b3
enzinxz <enzopontesdonascimento@gmail.com>
2026-06-09 16:37:54 -0300

    Aula 116 - Encerrado

---
+++ php/secoes/secao-6/aula-116/index.php
@@ -0,0 +1,17 @@
+<?php 
+
+ $x = 0;
+
+ switch($x) {
+
+    case 0:
+        echo "X é igual a 0 <BR>";
+        break;
+    case 1:
+        echo "X é igual 1 <BR>";
+        break;
+    default:
+        echo "X não é igual a nenhum dos valores mencionados <BR>";
+ }
+
+?>
