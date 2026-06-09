edd3ab6048aa2da10bbab70229bdb398daf0ee94
enzinxz <enzopontesdonascimento@gmail.com>
2026-06-09 15:06:43 -0300

    Desafio aula 110 - Encerrado

---
+++ php/desafios/secao-6-desafio/aula-110-desafio/index.php
@@ -0,0 +1,33 @@
+<?php 
+
+$str = "String";
+$num = 1;
+$bool = true;
+
+if (is_int($str)) {
+    echo "É um inteiro.";
+    echo "<BR>";
+} else {
+    echo "Não é um inteiro.";
+    echo "<BR>";
+}
+
+if (is_int($num)) {
+    echo "É um inteiro.";
+    echo "<BR>";
+} else {
+    echo "Não é um inteiro.";
+    echo "<BR>";
+}
+
+if (is_int($bool)) {
+    echo "É um inteiro.";
+    echo "<BR>";
+} else {
+    echo "Não é um inteiro.";
+    echo "<BR>";
+}
+
+
+
+?>
