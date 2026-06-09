3c1f382ab7930194a1e48c52e847f4894004d83a
enzinxz <enzopontesdonascimento@gmail.com>
2026-06-09 14:42:33 -0300

    Aula 105 - Encerrada

---
+++ php/secao-6/aula-105/index.php
@@ -0,0 +1,20 @@
+<?php 
+
+$num1 = 5;
+$num2 = 67;
+
+// False
+if ($num1 > $num2) {
+    echo "TRUE";
+} else {
+    echo "False";
+}
+
+// True
+if ($num1 < $num2) {
+    echo "TRUE";
+} else {
+    echo "False";
+}
+
+?>
