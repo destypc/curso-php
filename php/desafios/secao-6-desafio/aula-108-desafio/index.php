2f103ac8f75e6a5e1f01b003255cdf729c8c17a8
enzinxz <enzopontesdonascimento@gmail.com>
2026-06-09 15:00:27 -0300

    Desafio aula 109 - Encerrada

---
+++ php/desafios/secao-6-desafio/aula-108-desafio/index.php
@@ -0,0 +1,29 @@
+<?php
+
+$enzo = 15;
+$luis = 16;
+$erick = 21;
+
+if ($enzo >= 18) {
+    echo "Enzo é maior de idade, ele tem $enzo anos";
+    echo "<BR>";
+} else {
+    echo "Enzo é menor de idade, ele tem $enzo anos";
+    echo "<BR>";
+}
+
+if ($luis >= 18) {
+    echo "Luis é maior de idade, ele tem $luis anos";
+    echo "<BR>";
+} else {
+    echo "Luis é menor de idade, ele tem $luis anos";
+    echo "<BR>";
+}
+
+if ($erick >= 18) {
+    echo "Erick é maior de idade, ele tem $erick anos";
+    echo "<BR>";
+} else {
+    echo "Erick é menor de idade, ele tem $erick anos";
+    echo "<BR>";
+}
