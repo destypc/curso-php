024b5ed27fad8bb920e57496b963e0ef8d3250f6
enzinxz <enzopontesdonascimento@gmail.com>
2026-06-09 14:39:32 -0300

    Desafio aula 102 - Encerrada

---
+++ php/desafios/secao-5-desafio/aula-102-desafio/index.php
@@ -0,0 +1,23 @@
+<?php 
+
+$num1 = 5;
+$num2 = 67;
+
+if ($num1 > $num2) {
+    echo "TRUE";
+    echo "<BR>";
+} else {
+    echo "False";
+    echo "<BR>";
+}
+
+if ($num1 < $num2) {
+    echo "True";
+    echo "<BR>";
+} else {
+    echo "False";
+    echo "<BR>";
+}
+
+
+?>
