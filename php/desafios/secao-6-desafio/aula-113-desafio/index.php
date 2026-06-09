48c01804f56352bb168c1cdd664d51022d3895c5
enzinxz <enzopontesdonascimento@gmail.com>
2026-06-09 15:41:25 -0300

    Desafio aula 113 - Encerrada

---
+++ php/desafios/secao-6-desafio/aula-113-desafio/index.php
@@ -0,0 +1,15 @@
+<?php 
+
+$num1 = 51;
+$num2 = 31;
+
+$str1 = "Enzo";
+$str2 = "Jamilton";
+$res = null; 
+
+if (is_numeric($num1)) {
+    $res = $num1 * 2;
+}if ($res >= 100) {
+    echo "Esse número é maior que 100";
+}
+?>
