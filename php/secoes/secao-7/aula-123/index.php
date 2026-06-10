257b3b4865e909dd886a4f1fdddc0bc70a953238
enzinxz <enzopontesdonascimento@gmail.com>
2026-06-10 14:41:35 -0300

    Aula 123 - Encerrada

---
+++ php/secoes/secao-7/aula-123/index.php
@@ -0,0 +1,20 @@
+<?php 
+
+$i = 0;
+while ($i <= 10) {
+    echo "Loop externo $i <br>";
+
+    // Segundo contador
+    $j = 0;
+
+
+    while($j <= 5) {
+        echo "Loop interno $j <br>";
+
+        $j ++;
+    }
+
+    $i ++;
+}
+
+?>
