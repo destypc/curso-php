f867df6f3c3d50d022f21bcff2a450af306d0240
enzinxz <enzopontesdonascimento@gmail.com>
2026-06-10 15:01:06 -0300

    Aula 126 - Encerrada

---
+++ php/secoes/secao-7/aula-126/index.php
@@ -0,0 +1,18 @@
+<?php
+
+$j = 0;
+
+do {
+    echo "Testando do while $j <br>";
+
+    $j++;
+} while ($j < 10);
+
+$i = 20;
+echo "<BR>";
+
+do {
+    echo "Testando do while $i <br>";
+
+    $i--;
+} while ($i > 10);
