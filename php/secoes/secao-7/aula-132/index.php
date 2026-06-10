d50ba2d6c01bae2887a60fdae5f627eecc8fdcfa
enzinxz <enzopontesdonascimento@gmail.com>
2026-06-10 16:00:53 -0300

    Aula 134 - Desafio

---
+++ php/secoes/secao-7/aula-132/index.php
@@ -0,0 +1,13 @@
+<?php 
+
+$nomes = ["Jamilton", "Pedro", "Ana", "Maria"];
+
+foreach ($nomes as $nome) {
+    echo "O nome o índice atual é $nome <br>";
+
+    if ($nome == "Jamilton") {
+        echo "Opa <br>";
+    }
+}
+
+?>
