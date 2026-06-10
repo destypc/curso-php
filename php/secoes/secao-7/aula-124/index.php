aac0a45981e8adc7e24eaa1b8de32d86dc845d4c
enzinxz <enzopontesdonascimento@gmail.com>
2026-06-10 14:46:18 -0300

    Aula 125 - Encerrada

---
+++ php/secoes/secao-7/aula-124/index.php
@@ -0,0 +1,27 @@
+<?php
+
+$a = 10;
+
+while ($a > 0) {
+
+
+
+    if ($a === 5 || $a == 7) {
+        echo "Pulou a execução $a <br>";
+
+        $a --;
+
+        continue;
+        
+    }
+
+    if ($a == 2) {
+
+    echo "Terminando o loop breack $a <br>";
+
+    break;
+    }
+
+    echo "Executando o loop $a <br>";
+    $a--;
+}
