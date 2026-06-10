0148170c0af6b6a1a71d6478a83c6b0433862031
enzinxz <enzopontesdonascimento@gmail.com>
2026-06-10 15:06:00 -0300

    Aula 127 - Encerrada

---
+++ php/secoes/secao-7/aula-127/index.php
@@ -0,0 +1,18 @@
+<?php 
+
+
+
+for ($n = 0 ;$n <= 100 ; $n++) {
+
+    if ($n == 67) {
+        echo "Pagotti <br>";
+    }
+
+    if ($n == 97) {
+        break;
+    }
+
+    echo "Testando o for $n <br>";
+}
+
+?>
