93167a9e410cf2638fe2a47133500f282f5e81ec
enzinxz <enzopontesdonascimento@gmail.com>
2026-06-11 15:30:01 -0300

    Aula 144 - Finalizado

---
+++ php/secoes/secao-9/aula-144/index.php
@@ -0,0 +1,21 @@
+<?php 
+
+// Definindo função
+function soma () {
+
+    $a = 5;
+    $b = 4;
+
+    echo $a + $b . "<br>";
+
+}
+
+// Chamando / Invocando funcao
+soma();
+soma();
+
+
+// Funcao do php
+
+echo strtoupper("Testando função nativa do php");
+?>
