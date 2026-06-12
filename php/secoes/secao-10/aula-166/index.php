341acab6db4fe5047a5246f7097d4807b30be5f0
enzinxz <enzopontesdonascimento@gmail.com>
2026-06-12 16:55:26 -0300

    Aula 166 - Encerrado

---
+++ php/secoes/secao-10/aula-166/index.php
@@ -0,0 +1,15 @@
+<?php 
+
+    print("Imprimindo algo com print <br>");
+
+    $algo = "Algo";
+
+    print("Imprimindo $algo com print <br>");
+    
+    $eu = "Eu";
+    $sou  = "sou";
+    $o = "o";
+    $enzo = "Enzo.";
+
+    print ("$eu $sou $o $enzo")
+?>
