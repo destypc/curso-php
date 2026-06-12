6df86a568a093ef75ab736c8c280395576d0d302
enzinxz <enzopontesdonascimento@gmail.com>
2026-06-12 17:07:48 -0300

    Aula 167 - Encerrada

---
+++ php/secoes/secao-10/aula-167/index.php
@@ -0,0 +1,20 @@
+<?php 
+
+    $nome = "Enzinxz";
+
+    // %s -> string
+
+    printf("O nome é %s <br>", $nome);
+
+    // %d -> inteiro
+
+    $int = 67;
+
+    printf("O número mais resenha é %d e o outro é %d <br> ", $int, 6767);
+    
+    // %f -> float
+
+    $flt = 12.5;
+
+    printf("Float %.2f e %.1f <br> ", $flt, 19.88)
+?>
