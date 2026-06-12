81b2163f323916f6a3e7074116c8fbf554fa2954
enzinxz <enzopontesdonascimento@gmail.com>
2026-06-12 16:11:52 -0300

    Aula 163 - Encerrada

---
+++ php/secoes/secao-10/aula-163/index.php
@@ -0,0 +1,10 @@
+<?php 
+$nome = "Enzo";
+$idade = 15;
+$profissao = "desenvolvedor";
+
+echo "Eu sou o $nome, tenho $idade anos e atuo como $profissao. <br><br>";
+
+echo "Eu sou o {$nome}, tenho {$idade} anos e atuo como {$profissao}. <br><br>";
+
+?>
