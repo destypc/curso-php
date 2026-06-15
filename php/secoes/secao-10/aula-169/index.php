52ae59a2b9a6cff8e283cdfba9c59a0837937c7e
enzinxz <enzopontesdonascimento@gmail.com>
2026-06-15 14:41:11 -0300

    Aula 171 - Encerrada

--- php/secoes/secao-10/aula-169/index.php
+++ php/secoes/secao-10/aula-169/index.php
@@ -5,7 +5,6 @@ $str = "Esta é uma string muito grande, ela tem vários caracteres";
 for ($i = 0 ; $i < strlen($str) ; $i++) {
     
     echo "$str[$i] <br>";
-
 }
 
 ?>
---
+++ php/secoes/secao-10/aula-170/index.php
@@ -0,0 +1,10 @@
+<?php 
+
+$str1 = "    Matheus    ";
+
+echo "Esta é string 1: $str1.<br>";
+
+$str1Limpa = trim($str1);
+
+echo "Esta é string 1: $str1Limpa.<br>";
+?>
