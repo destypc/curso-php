5bbf9bfd6a3200dbb7c7656d921e19d53eb55858
enzinxz <enzopontesdonascimento@gmail.com>
2026-06-15 14:48:15 -0300

    Aula 172 - Encerrado

--- php/secoes/secao-10/aula-171/index.php
+++ php/secoes/secao-10/aula-171/index.php
@@ -7,4 +7,5 @@ echo "Esta é string 1: $str1.<br>";
 $str1Limpa = trim($str1);
 
 echo "Esta é string 1: $str1Limpa.<br>";
+
 ?>
---
+++ php/secoes/secao-10/aula-172/index.php
@@ -0,0 +1,13 @@
+<?php 
+
+$str = "Esta string está em caixa baixa <br>";
+
+echo strtoupper($str);
+
+$str1 = "E ESTA AQUI EM CAIXA ALTA";
+
+echo strtolower($str1);
+
+
+
+?>
