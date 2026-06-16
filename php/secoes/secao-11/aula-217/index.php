11e8f2ca06c2f7f25f8faa50386f2ded91f30888
enzinxz <enzopontesdonascimento@gmail.com>
2026-06-16 17:00:08 -0300

    Aula 218 - Encerrado

---
+++ php/secoes/secao-11/aula-217/index.php
@@ -0,0 +1,11 @@
+<?php 
+
+$a = range(1,100);
+
+$soma = array_sum($a);
+
+print_r($a);
+echo "<br>";
+echo $soma;
+
+?>
---
+++ php/secoes/secao-11/aula-218/index.php
@@ -0,0 +1,11 @@
+<?php 
+
+$arr1 = [1, 2, 3];
+$arr2 = [10, 40, 90];
+$arr3 = [50.3, 80.5, 3.1];
+
+$arrMerge = array_merge($arr1, $arr2, $arr3);
+
+print_r($arrMerge);
+
+?>
