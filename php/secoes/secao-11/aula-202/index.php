2bef23660b64ad614a9d643f3ca742af183f64de
enzinxz <enzopontesdonascimento@gmail.com>
2026-06-16 14:16:57 -0300

    Aula 202 - Encerrada

---
+++ php/secoes/secao-11/aula-202/index.php
@@ -0,0 +1,17 @@
+<?php
+
+$arr = [
+    'nome' => 'Enzo',
+    'idade' => 15,
+];
+
+if (array_key_exists("nome", $arr)) {
+
+    echo "A key existe dentro do array";
+}
+
+if (isset($arr,['teste'])) {
+
+    echo "A key existe dentro do array";
+    
+}
