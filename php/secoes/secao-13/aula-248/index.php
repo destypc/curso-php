a796dc1b9e3d837f4f7c7077b81b34ae28846507
enzinxz <enzopontesdonascimento@gmail.com>
2026-06-16 17:43:38 -0300

    Aula 249 - Encerrada

---
+++ php/secoes/secao-13/aula-248/index.php
@@ -0,0 +1,12 @@
+<?php 
+
+$d = date ('d/m/y');
+echo $d . "<br><br>";
+
+$d2 = date('D/M - Y');
+echo $d2 . "<br><br>";
+
+$d3 = date('d/m/Y');
+echo $d3 . "<br><br>"
+
+?>
---
+++ php/secoes/secao-13/aula-249/index.php
@@ -0,0 +1,10 @@
+<?php 
+
+    $cincoDias = strtotime("5 days");
+
+    $dataAtualMaisCinco = date('d/m/Y' , $cincoDias);
+
+    echo $dataAtualMaisCinco . "<br>"
+    
+
+?>
