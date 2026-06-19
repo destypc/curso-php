9f5b4aa5d034872bdf84cc2be95eee105f35fe3d
enzinxz <enzopontesdonascimento@gmail.com>
2026-06-19 15:22:47 -0300

    Orgazinda os nomes de cada pasta

---
+++ php/secoes/secao-14/aula-274/index.php
@@ -0,0 +1,24 @@
+<?php 
+
+    session_start();
+    
+    // print_r($_SESSION);
+
+    $_SESSION['nome'] = "Enzo";
+    
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aula-274</title>
+</head>
+
+<body>
+    <h1>Olá session</h1>
+</body>
+
+</html>
