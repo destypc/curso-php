6549ae32d95fcb0acd453e1aaadb5fc0cbae7a36
enzinxz <enzopontesdonascimento@gmail.com>
2026-06-23 17:34:16 -0300

    Aula 5 - Encerrada

--- php-mysqli/5_selecionando_dados/index.php
+++ php-mysqli/5_selecionando_dados/index.php
@@ -1,5 +1,22 @@
-<?php 
+<?php
 
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db = "cursophp";
 
+$conn = new mysqli($host, $user, $pass, $db);
 
-?>
+// ASSUNTO DA AULA
+
+$q = "SELECT * FROM itens";
+
+// UM RESULTADO
+$result = $conn->query($q);
+
+$itens = $result->fetch_assoc();
+
+// TODOS OS RESULTADOS
+$itens = $result->fetch_all();
+
+print_r($itens);
